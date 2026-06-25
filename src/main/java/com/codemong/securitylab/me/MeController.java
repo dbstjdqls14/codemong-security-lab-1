d5ab4019806c8cab48af2c96b271676d808c7e62
YunSungBin <dbstjdqls14@naver.com>
2026-06-26 04:06:53 +0900

    Initialize answer code: src/main/java/com/codemong/securitylab/me/MeController.java

---
+++ src/main/java/com/codemong/securitylab/me/MeController.java
@@ -0,0 +1,22 @@
+package com.codemong.securitylab.me;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MeController {
+
+    private final MeService meService;
+
+    public MeController(MeService meService) {
+        this.meService = meService;
+    }
+
+    @GetMapping("/me")
+    public MeResponse me(Authentication authentication) {
+        return meService.me(authentication);
+    }
+}
