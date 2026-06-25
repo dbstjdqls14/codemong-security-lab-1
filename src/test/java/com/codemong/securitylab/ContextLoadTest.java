9ff485871adc1d0481777e252378414d257f8926
YunSungBin <dbstjdqls14@naver.com>
2026-06-26 04:07:11 +0900

    Initialize answer code: src/test/java/com/codemong/securitylab/ContextLoadTest.java

---
+++ src/test/java/com/codemong/securitylab/ContextLoadTest.java
@@ -0,0 +1,14 @@
+package com.codemong.securitylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ContextLoadTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
